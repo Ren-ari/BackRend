6bb7bb9d2314a21d1d5fb89ca2a0c545c2988f19
Renari <robertokk@naver.com>
2024-11-01 17:09:55 +0900

    렌친듀오 서버 제작

---
+++ server.js
@@ -0,0 +1,11 @@
+
+const express = require('express')
+const app = express()
+
+app. listen(8080, () => {
+  console.log('http://localhost:8080에서 서버 실행중')
+})
+
+app.get('/',(요청, 응답) => {
+  응답.send('반갑다')
+})
